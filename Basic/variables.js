7a9f0ab4d734991aee5cc3b875a6501ed257debf
RahulYadav2023 <134481127+RahulYadav2023@users.noreply.github.com>
2024-10-15 08:40:05 +0000

    VariablesDefines

---
+++ Basic/variables.js
@@ -0,0 +1,15 @@
+const Id = 12345;
+let Name = "Rahul Yadav";
+var Email = "rahul@gmail.com"
+
+AccountId = 1234;
+// Id = 4 Not Assign another value.
+Name = "Raman Ray";
+Email = "raman@gmail.com"
+AccountId = 2;
+
+console.log("Id:",Id);
+console.log("Name:",Name);
+console.log("EmailId:",Email);
+console.log("AccountId:",AccountId);
+console.table([Id,AccountId,Name,Email])
