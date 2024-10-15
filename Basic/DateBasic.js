82e27c92628acb8cc787fc718c40273a12dd4f26
RahulYadav2023 <134481127+RahulYadav2023@users.noreply.github.com>
2024-10-15 13:23:57 +0000

    DateBasic

---
+++ Basic/DateBasic.js
@@ -0,0 +1,10 @@
+const myDate = new Date();
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+const myDate1 = new Date("2024-01-15");
+console.log(myDate1);
